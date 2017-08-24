855333f5a46d5ad4abf06196da146269581b058a
Bryce Pilcher <brycepilchergithub@gmail.com>
2017-08-24 10:34:00 -0400

    modified index

--- src/App.js
+++ src/App.js
@@ -8,10 +8,10 @@ class App extends Component {
       <div className="App">
         <div className="App-header">
           <img src={logo} className="App-logo" alt="logo" />
-          <h2>Welcome to React</h2>
+          <h2>Round-Robin Expression</h2>
         </div>
         <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
+          A place of expression in which each post is given an equal amount of time and then discarded.
         </p>
       </div>
     );
